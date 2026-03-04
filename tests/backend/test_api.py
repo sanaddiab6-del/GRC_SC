50cf8d2855a46feadde737a0a619c6b9cd4188d3
sanaddiab6-del <sanaddiab6@gmail.com>
2026-03-04 15:06:06 +0300

    fix Backend Tests.2

--- tests/backend/test_api.py
+++ tests/backend/test_api.py
@@ -1,8 +1,7 @@
 # Backend Tests
 import pytest
 from httpx import AsyncClient, ASGITransport
-from main import app
-
+from src.backend.main import app
 
 @pytest.mark.asyncio
 async def test_health_check():
